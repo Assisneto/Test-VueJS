aa1d3d675576760c0ef316d59a23541bb42a48f7
Assisneto <netodamasceno12@gmail.com>
2019-02-12 15:30:50 -0300

    components and scoped

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app">
-   <h1>{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
 
@@ -12,7 +15,12 @@
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
 export default {
+    components: {
+
+    'meu-painel': Painel
+  },
 
   data () {
     return {
@@ -31,5 +39,27 @@ export default {
 </script>
 
 <style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width : 100%;
+  }
 
 </style>
