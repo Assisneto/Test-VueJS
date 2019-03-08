0d223703071bbac2a6b3ddac661d1c5fce79ddb7
Assisneto <netodamasceno12@gmail.com>
2019-03-08 09:45:22 -0300

    lazy loading and code splitting

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
-import Cadastro from './components/cadastro/Cadastro.vue';
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
+
 import Home from './components/home/Home.vue';
 
 export const routes = [
-    { path: '', name: 'home', component: Home, titulo: 'Home', menu: true},
-    { path: '/cadastro', name: 'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true},
-    { path: '/cadastro/:id', name: 'altera', component: Cadastro, titulo: 'Cadastro', menu: false},
-    { path: '*', component: Home, menu: false}
+  { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
+  { path: '/cadastro', name: 'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true },
+  { path: '/cadastro/:id', name: 'altera', component: Cadastro, titulo: 'Cadastro', menu: false },
+  { path: '*', component: Home, menu: false }
 ];
